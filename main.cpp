087b3cc69e22b8a652c08646a8b31c1b52663d89
Dmitryy2 <95528970+Dmitryy2@users.noreply.github.com>
2021-12-16 22:19:40 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <serverthread.h>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+
+
+    ServerThread servThread;
+
+    servThread.run();
+    w.show();
+
+    return a.exec();
+}
